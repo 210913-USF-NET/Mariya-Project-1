d70fc810b4c02abe651c635fe4a8bff4c241339d
MariyaBukh <85184644+MariyaBukh@users.noreply.github.com>
2021-10-07 10:50:20 -0400

    added layout

--- WebUI/Models/ProductViewscs.cs
+++ WebUI/Models/ProductViewscs.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Models;
 
 namespace WebUI.Models
     {
-    public class ProductViewscs
+    public class ProductVM
         {
-        //string genre
-        //list prod
+        public List<string> Genres { get; set; }
+        public List<Product> Products { get; set; }
         }
     }
